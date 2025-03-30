da551869f37cefb45fab50e418b1302bb883fcca
VadimBaraa <rentsvadim@gmail.com>
2025-03-30 16:17:24 +0300

    Add test text 1

--- obj/Debug/net9.0/SocialNet.AssemblyInfo.cs
+++ obj/Debug/net9.0/SocialNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcafca299f9c54341b5824a35eaca4756cbf6470")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
